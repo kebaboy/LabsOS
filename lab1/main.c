726581361e88b5fab2a7c128e0af0d071fda5c99
ya-grigorii <grisha_mgn@bk.ru>
2025-01-01 20:21:40 +0300

    add -a flag

--- lab1/main.c
+++ lab1/main.c
@@ -2,23 +2,26 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
+#include <unistd.h>
 
 int compare(const void *a, const void *b) {
     return strcmp(*(const char **)a, *(const char **)b);
 }
 
-void list_dir(const char *path) {
+void list_dir(const char *path, int show_all) {
     struct dirent *entry;
     DIR *dir = opendir(path);
 
     if (!dir) {
         fprintf(stderr, "Error: Cannot open directory '%s'\n", path);
-        return;
+        exit(EXIT_FAILURE);
     }
 
     char **files = NULL;
     int count = 0;
     while ((entry = readdir(dir)) != NULL) {
+        if (!show_all && entry->d_name[0] == '.')
+            continue;
         files = realloc(files, sizeof(char *) * (count + 1));
         files[count] = strdup(entry->d_name);
         count++;
@@ -35,7 +38,21 @@ void list_dir(const char *path) {
 }
 
 int main(int argc, char *argv[]) {
-    const char *path = (argc > 1) ? argv[1] : ".";
-    list_dir(path);
+    int opt;
+    int show_all = 0;
+
+    while ((opt = getopt(argc, argv, "a")) != -1) {
+        switch (opt) {
+            case 'a':
+                show_all = 1;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-a] [directory]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    const char *path = (optind < argc) ? argv[optind] : ".";
+    list_dir(path, show_all);
     return 0;
 }
